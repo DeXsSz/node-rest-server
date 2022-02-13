bac0075ce4e23f6a11921fb78b7da2113f8edb46
Agustin <agusstingalva@gmail.com>
2022-02-13 17:48:32 -0300

    variable de entorno heroku

--- server/config/config.js
+++ server/config/config.js
@@ -8,14 +8,13 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 let urlDB;
 
-urlDB = 'mongodb+srv://gatoloco:gatitoloco@cluster0.ytwv5.mongodb.net/cafe'
-// if(process.env.NODE_ENV === 'dev'){
-//     urlDB = 'mongodb://localhost:27017/cafe';
-// }else{
-// }
+if(process.env.NODE_ENV === 'dev'){
+        urlDB = 'mongodb://localhost:27017/cafe';
+    }else{
+    urlDB = process.env.MONGO_URI;
+}
 /// pass and username of database of mongodb Atlas
-//user: gatoloco pass: gatitoloco
-//mongodb+srv://gatoloco:gatitoloco@cluster0.ytwv5.mongodb.net/test
+
 
 
 process.env.URLDB = urlDB
